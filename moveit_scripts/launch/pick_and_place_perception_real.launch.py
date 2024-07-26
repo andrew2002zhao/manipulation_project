f5766dcc5cdaeced0c0fd3ce1f1ff5bcb2270588
Andrew Zhao <andrew2002zhao@gmail.com>
2024-07-26 17:12:22 +0000

    corrected launch file problems

--- moveit_scripts/launch/pick_and_place_perception_real.launch.py
+++ moveit_scripts/launch/pick_and_place_perception_real.launch.py
@@ -6,7 +6,7 @@ from moveit_configs_utils import MoveItConfigsBuilder
 
 def generate_launch_description():
 
-    moveit_config = MoveItConfigsBuilder("name", package_name="my_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("ur3e", package_name="real_moveit_config").to_moveit_configs()
 
     
     container = ComposableNodeContainer(
